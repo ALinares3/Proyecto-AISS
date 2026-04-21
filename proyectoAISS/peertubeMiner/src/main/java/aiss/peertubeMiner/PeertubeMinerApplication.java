fb14824c35ce614ad2b9b952cb838566ec0f4da4
ALinares3 <alelinaresrin@gmail.com>
2026-04-21 12:31:19 +0200

    Add files via upload

---
+++ proyectoAISS/peertubeMiner/src/main/java/aiss/peertubeMiner/PeertubeMinerApplication.java
@@ -0,0 +1,21 @@
+package aiss.peertubeMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class PeertubeMinerApplication {
+
+	@Bean
+	public RestTemplate builder(RestTemplateBuilder builder){
+		return builder.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeertubeMinerApplication.class, args);
+	}
+
+}
---
+++ proyectoAISS/peertubeMiner/src/test/java/aiss/peertubeMiner/PeertubeMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.peertubeMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeertubeMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
